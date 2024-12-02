ec0f57981f10e5c9d8695551f9f5b02ff20b2759
Stanislav1337 <Stasgrisha2@yandex.ru>
2024-12-02 23:57:30 +0300

    String_learn

---
+++ BigInt.js
@@ -0,0 +1,28 @@
+/*let num = 9007199254740991;
+console.log(num);       
+console.log(num + 1);   
+console.log(num + 2); 
+
+console.log("========");
+
+console.log(Number.MIN_VALUE);   // 5e-324
+console.log(Number.MAX_VALUE);  // 1.7976931348623157e+308
+
+console.log("========");
+
+const value = 9223372036854775801;
+console.log(value);   // 9223372036854776000*/
+
+/* Для определения числа как значения типа BigInt 
+в конце числа добавляется суффикс n*/
+/*let dimension = 19007n; 
+const value = 2545n;
+
+const num = 9223372036854775801n;
+console.log(num);   // 9223372036854775801n*/
+
+/*Тип Boolean представляет булевые 
+или логические значения true (верно) и false (ложно)
+
+const isAlive = true;
+const isDead = false;*/
